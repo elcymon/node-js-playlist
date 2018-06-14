dac9907f0f4013cee17ce18d3f18370d7b279bf7
Simon Obute <elcymon.coding@gmail.com>
2018-06-15 00:51:25 +0100

    #4 - #6

--- mine-practice/app.js
+++ mine-practice/app.js
@@ -1,1 +1,46 @@
-console.log('hey ninjas');
+//#6 Modules and require
+//for dividing code into logical modules i.e. separate files.
+//our applications can be divided into logical files.
+
+var counter = require('./count');//what is exported is returned and stored in counter variable.
+
+console.log(counter(['shaun','crystal','ryu']));
+//#4
+// var time = 0;
+// var timer = setInterval(function(){
+//     time += 2;
+//     console.log(time + ' seconds have passed');
+//     if (time > 5){
+//         clearInterval(timer);
+//     }
+// },2000);
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+//#5 Function Expressions
+function callFunction(fun){
+    fun();
+}
+//normal function statement
+
+function sayHi(){
+    console.log('Hi');
+}
+
+sayHi();
+
+//function expression is assigning a variable to an anonymous function
+
+var sayBye = function(){
+    console.log('bye');
+
+};
+
+sayBye();
+
+callFunction(sayBye);
+
+callFunction(sayHi);
