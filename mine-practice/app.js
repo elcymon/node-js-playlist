f8f3c1efbe74d3f548d2ad3c907ef910cbaa2de7
Simon Obute <elcymon.coding@gmail.com>
2018-06-15 21:37:37 +0100

    #8

--- mine-practice/app.js
+++ mine-practice/app.js
@@ -1,6 +1,37 @@
-//#7
-var stuff = require('./stuff');//what is exported is returned and stored in counter variable.
+//#8 Events module
 
-console.log(stuff.counter(['shaun','crystal','ryu']));
-console.log(stuff.adder(4,5));
-console.log(stuff.adder(stuff.pi,5));
+//NODEJS comes with some core modules built into nodejs
+var events = require('events');
+var util = require('util');
+
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person,events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryu = new Person('Ryu');
+
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on('speak',function(msg){
+        console.log(person.name + ' said ' + msg);
+    });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
+
+//You can create custom events and work on them.
+
+// var myEmitter = new events.EventEmitter();
+
+// //wire up events to it
+// myEmitter.on('someEvent', function(msg){
+//     console.log(msg);
+// });
+
+// myEmitter.emit('someEvent','the event was emitted');
