673ecc34618a44e0c0ead3c10470cd2e2645e933
elcymon <elcymon.coding@gmail.com>
2018-06-22 22:13:16 +0100

    todo app development created view ejs file #33

--- mine-practice/app.js
+++ mine-practice/app.js
@@ -1,51 +1,36 @@
-//template engines allow us to load html, javascript etc on our response message to the client
-//partial views are needed to create views that appear in multiple template files
-//examples are navigation views, so that we do not have to hard code it into 
-//every view that they appear in 
-//also, changing it in one section does not entail changing in multiple places
 var express = require('express');
-
+var bodyParser = require('body-parser');
 //To give us access to the numerous cool features/functions in express
 //such as routing and templating.
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 //set the view engine you want to use
 app.set('view engine', 'ejs');
 
-//app also gives us access to http verbs or methods: get, post, delete and put requests.
-// app.get('/',function(req,res){
-//     res.sendFile(__dirname+'/index.html');
-// });
-
-// app.get('/contact',function(req,res){
-//     res.sendFile(__dirname+'/contact.html');
-// });
-
-// //route parameters
-// app.get('/profile/:name', function(req,res){
-//     res.send('You viewing the profile of ' + req.params.name);
-// });
-
-//middle ware is any code between request and response.
-//we can use it to serve up static files like style sheets, images etc, without 
-//having to create separate routes for them.
-//Express has its own inbuilt middle ware we can use.
-// app.use('/assets',function(req,res,next){//matches everythin after /assets.
-//     console.log(req.url);
-//     next();//needed at the end of the middle ware code to move on to other requests (that is other middle ware or route requests)
-// });
 
 //'/assets' matches the route. name can be changed if you want
 //express.static('assets') matches the folder. name can be changed if you want
 app.use('/assets',express.static('assets'));//inbuilt express function to handle static assets points to 'assets' folder
+
 //rendering views
 app.get('/',function(req,res){
     res.render('index');
 });
 
 app.get('/contact',function(req,res){
-    res.render('contact');
+    //console.log(req.query);
+    res.render('contact', {qs: req.query});
 });
+
+app.post('/contact',urlencodedParser, function(req,res){
+    console.log(req.body);
+
+    //console.log(req.query);
+    res.render('contact-success', {data: req.body});
+});
+
 app.get('/profile/:name', function(req,res){
     var data = {
         age: 29,
