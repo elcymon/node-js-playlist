082e0661c47dcf79332b84de0f65d424023f8078
elcymon <elcymon.coding@gmail.com>
2018-06-24 07:41:58 +0100

    connecting to mlab database and sending dummy data

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,4 +1,23 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb://test123:test123@ds163700.mlab.com:63700/elcymon-todo');
+
+//you need to create a schema - i.e. blueprint for our data.
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+//create a model based on the schema
+var Todo = mongoose.model('Todo', todoSchema);
+
+//testing
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+                if(err) throw err;
+                console.log('item saved');
+            });
+
 var data = [
     {item: 'get milk'},
     {item: 'walk dog'},
