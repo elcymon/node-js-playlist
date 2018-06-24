2a5ef17eca78bc43f8d302acea19967d3f230ef0
elcymon <elcymon.coding@gmail.com>
2018-06-24 08:23:16 +0100

    #37 connecting the todo app to the mlab database

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -12,37 +12,52 @@ var todoSchema = new mongoose.Schema({
 //create a model based on the schema
 var Todo = mongoose.model('Todo', todoSchema);
 
-//testing
-var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-                if(err) throw err;
-                console.log('item saved');
-            });
+// //testing
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//                 if(err) throw err;
+//                 console.log('item saved');
+//             });
 
-var data = [
-    {item: 'get milk'},
-    {item: 'walk dog'},
-    {item: 'kick some coding ass'}
-];
+// var data = [
+//     {item: 'get milk'},
+//     {item: 'walk dog'},
+//     {item: 'kick some coding ass'}
+// ];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo', function(req,res){
-        res.render('todo',{todos: data});
+        //get data from mongoDB and pass it to the view
+        Todo.find({}, function(err,data){
+            if (err) throw err;
+            res.render('todo',{todos: data});
+        });//retrieve all the items. Finding a specific item, we need to pop in a specific item e.g. {item: "buy flowers"}
+        
     });
 
     app.post('/todo', urlencodedParser, function(req,res){
-        data.push(req.body);
-        console.log(data);
-        res.json(data);
+        //get data from the view and add it to the mongodb
+        var newTodo = Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
 
+        });
+        // data.push(req.body);
+        //console.log(data);
+        
     });
 
     app.delete('/todo/:item', function(req,res){
-        data = data.filter(function(todo){
-            console.log(todo);
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        //delete the requested item from mongoDB
+        Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
+        // data = data.filter(function(todo){
+        //     console.log(todo);
+        //     return todo.item.replace(/ /g, '-') !== req.params.item;
+        // });
+        // res.json(data);
     });
     
 };
